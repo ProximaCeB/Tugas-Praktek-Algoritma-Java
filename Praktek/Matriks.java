87a67f8acb0b84e428e680531c935090c1bd498a
andy kasa <145452247+ProximaCeB@users.noreply.github.com>
2023-11-30 09:21:53 +0700

    Menambahkan materi Matriks

---
+++ Praktek/Matriks.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class MatrixMath {   
+public static void main( String[] args )
+ {  Scanner keyboard = new Scanner(System.in);		
+	int i,j;
+	int n=3;
+	int[][] A = new int[n][n];  
+	//input matrix
+	System.out.println("Input Matrix A:");
+	for(i=0; i<n; i++ )
+	{		for(j=0; j<n; j++ )
+			{
+			System.out.print("A["+(i+1)+"]["+(j+1)+"]:");
+			A[i][j] = keyboard.nextInt();
+			}
+	}
+	
+	//mencetak matrix
+	System.out.println("Cetak Matrix A:");
+	for(i=0; i<n; i++ )
+	{	System.out.print("| ");
+		for(j=0; j<n; j++ )
+			{
+			   System.out.print(A[i][j]+" ");
+			}
+		System.out.print("|");	
+		System.out.println();
+	}
+	
+ }//end main
+}//end class
